308d37722b2859dacb67fc22d88cfaa204db494a
kadir kaya <kadirkaya.13@outlook.com.tr>
2021-01-17 00:37:42 +0300

    Add project files.

---
+++ WebApplicationFirstTry/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationFirstTry.Controllers
+{
+    public class StudentController : Controller
+    {
+          public ActionResult Show()
+            {
+                 ViewBag.MM = "hello"; 
+                return View();
+            }
+    }
+}
---
+++ WebApplicationFirstTry/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationFirstTry.MvcApplication" Language="C#" %>
